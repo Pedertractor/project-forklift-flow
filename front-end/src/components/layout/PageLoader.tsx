5a6a3b17df6a8dff67eb23a9f38eaee66e415349
Vanderlei1010 <vanderlei.junior@ptractor.com.br>
2026-05-28 15:57:13 -0300

    Update PageLoader component to use responsive height class for improved layout consistency

--- front-end/src/components/layout/PageLoader.tsx
+++ front-end/src/components/layout/PageLoader.tsx
@@ -2,7 +2,7 @@ import AccordionLoader from '../accordionLoader/accordion-loader';
 
 export function PageLoader() {
   return (
-    <div className="flex min-h-[40vh] items-center justify-center">
+    <div className="flex min-h-svh items-center justify-center">
       <AccordionLoader />
     </div>
   );
